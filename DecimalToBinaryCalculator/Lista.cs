60266568618e766fe256235b5bda62670ada269d
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2021-12-18 11:51:26 +0100

    Add files via upload

---
+++ DecimalToBinaryCalculator/Lista.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinaryCalculator
+{
+    class Lista
+    {
+        public static void FillBinaryList(double input)
+        {
+            List<int> BinaryList = new List<int>();
+
+            for (int i = 1; i < input; i *= 2)
+            {
+                BinaryList.Add(i);
+            }
+
+            List<int> BinaryNr = new List<int>();
+
+            while (BinaryList.Count > 0)
+            {
+                if (BinaryList[^1] <= input)
+                {
+                    BinaryNr.Add(1);
+                    input = input - BinaryList[^1];
+                    BinaryList.RemoveAt(BinaryList.Count - 1);
+                }
+                else
+                {
+                    BinaryNr.Add(0);
+                    BinaryList.RemoveAt(BinaryList.Count - 1);
+                }
+
+            }
+
+            Console.WriteLine();
+
+            foreach (var e in BinaryNr)
+            {
+                Console.Write(e);
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+
+}
+
---
+++ DecimalToBinaryCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecimalToBinaryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = 0;
+            
+            while (input<1)
+            {
+                Console.WriteLine("Write a number between 1 and whatever");
+
+                double.TryParse(Console.ReadLine(), out input);
+            }
+
+           
+
+            Lista.FillBinaryList(input);
+
+            
+
+
+        }
+    }
+}
